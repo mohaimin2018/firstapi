24f06fbabff4776e42f28789dd194cfe619e0113
Md. Mohai Minul Islam <mohai.cse@gmail.com>
2022-09-18 00:16:44 +0600

    views and urls are updated.

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart import views
+from quickstart import views
 
 
 router = routers.DefaultRouter()
